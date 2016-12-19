e1b9087cfc6fbee56038e023ead29ff6263eea0a
DeWinters <derekwwinters@gmail.com>
2016-12-19 15:50:07 +0000

    Add project files.

---
+++ dw6n1449/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dw6n1449
+{
+    class Card
+    {
+        private int id;
+        private int val;
+        private string face;
+    }
+}
